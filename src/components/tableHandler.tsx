2b4623c3bf061eb3c1800972be4c1b303c196388
Kusalava0 <dkb03122001@gmail.com>
2023-05-16 19:30:07 +0530

    ui changes

--- src/components/tableHandler.tsx
+++ src/components/tableHandler.tsx
@@ -303,7 +303,7 @@ const TableHandler: React.FC = () => {
                                         <td>
                                             <div className='edit-delete flex items-center justify-center'>
                                                 <button onClick={() => handleDelete(row.original.id)}
-                                                    className="inline-flex items-center justify-center w-8 h-8 mr-2 text-black-100 transition-colors duration-150  rounded-3xl focus:shadow-outline hover:bg-red-600">
+                                                    className="inline-flex items-center justify-center w-8 h-8 mr-2 text-black-100 transition-colors duration-150  rounded-3xl focus:shadow-outline hover:bg-red-600 hover:text-white">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                     </svg></button>
@@ -314,7 +314,7 @@ const TableHandler: React.FC = () => {
                                                             role: ['Admin', 'Sales Rep', 'Sales Lead'][Math.floor(Math.random() * 3)]
                                                         })
                                                     }
-                                                    className="flex p-2.5 bg-white rounded-xl hover:rounded-3xl hover:bg-gray-200 transition-all duration-300 text-black">
+                                                    className="inline-flex items-center justify-center w-8 h-8 mr-2 text-black-100 transition-colors duration-150  rounded-3xl focus:shadow-outline hover:bg-black hover:text-white">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                     </svg>
